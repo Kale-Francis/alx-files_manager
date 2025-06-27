7c5f28544dac8d76309319e61030b8487fe4e38b
Kale-Francis <kalefrancis989@gmail.com>
2025-06-27 02:09:03 +0000

    Adde the first task file that Creates a RedisClient class to interact with Redis for storing temporary data

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.error('Redis Client Error:', err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return await getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    await setAsync(key, duration, value);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
